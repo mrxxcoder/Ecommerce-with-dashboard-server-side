68feeab7461af0aaafaf62670b0a9ac460eee55a
lowkeylu <ahmedsamy446x@gmail.com>
2024-01-08 02:44:27 +0200

    first commit

---
+++ config/plugins.js
@@ -0,0 +1,18 @@
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: "da8gmo7mr",
+        api_key: "921767139374472",
+        api_secret: "IrkASEjBuv9rDWQkvIdIts1K_iA",
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+  // ...
+});
